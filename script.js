40f7795c942d281b6ea6cf3acfba31c3cde198e9
Gabriel Iuri <inotcheou1@yahoo.com>
2023-04-27 15:42:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const BG_URL = "https://image.tmdb.org/t/p/original";
 
+
 const most_popular =
 	BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const top_rated =
